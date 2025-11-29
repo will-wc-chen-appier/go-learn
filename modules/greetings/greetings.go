7c881c977c3ffd6173d75387976ea36b1bb69b25
chen weichen <newsforcwc@gmail.com>
2025-11-29 14:12:47 +0800

    [feat] passing errors

--- modules/greetings/greetings.go
+++ modules/greetings/greetings.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// if no name was given, return an error
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
