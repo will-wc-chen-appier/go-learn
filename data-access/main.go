ff169ac853427ea47da8c8a38269bc7e84df1344
chen weichen <newsforcwc@gmail.com>
2025-12-06 16:25:17 +0800

    [feat] one line query

--- data-access/main.go
+++ data-access/main.go
@@ -50,6 +50,12 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Albums found: %v\n", albums)
+
+	album, err := albumById(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", album)
 }
 
 // albumsByArtist queries for albums that have the specified artist name.
@@ -75,4 +81,16 @@ func albumsByArtist(name string) ([]Album, error) {
 		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
 	}
 	return albums, nil
+}
+
+func albumById(id int64) (Album, error) {
+	var alb Album
+	row := db.QueryRow("select * from album where id=?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return alb, nil
 }
