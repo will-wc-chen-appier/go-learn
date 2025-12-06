a59c1338bae49cafa8d7fb14d453552ce0efd19e
chen weichen <newsforcwc@gmail.com>
2025-12-06 17:50:30 +0800

    [feat] init with gin

---
+++ web-service-gin/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float32 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Aloha", Artist: "Will", Price: 100.2},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.Run("localhost:8080")
+}
