8cd97a6d1cc017df1c245eae672ceeceeff9729d
chen weichen <newsforcwc@gmail.com>
2025-12-06 15:34:47 +0800

    [feat] data-access

--- data-access/main.go
+++ data-access/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 	"os"
 
-	"github.com/go-sql-driver/mysql/"
+	"github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
@@ -12,4 +14,21 @@ var db *sql.DB
 func main() {
 	cfg := mysql.NewConfig()
 	cfg.User = os.Getenv("DBUSER")
+	cfg.Passwd = os.Getenv("DBPASS")
+	cfg.Net = "tcp"
+	cfg.Addr = "127.0.0.1:3306"
+	cfg.DBName = "recordings"
+
+	// Get a database handle
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
 }
