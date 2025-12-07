831db3f179c77947c2f5a23e0af84e5dbef3705d
chen weichen <newsforcwc@gmail.com>
2025-12-07 21:03:05 +0800

    [feat] array declaration

--- playground/array.go
+++ playground/array.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func arrayTest() {
-	b := [...]int{100, 3: 400, 500}
+	b := [...]int{100, 3: 400, 2: 500} // [100, 0, 500, 400]
 	fmt.Println("idx: ", b)
 	fmt.Println("idx: ", len(b))
 }
